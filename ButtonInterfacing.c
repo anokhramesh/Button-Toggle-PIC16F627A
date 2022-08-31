a6ab2b6c51c2d18ae12bd313427d49a1e6fc5c29
Anokhautomation <67797819+anokhramesh@users.noreply.github.com>
2022-08-31 11:21:14 +0400

    Add files via upload

--- ButtonInterfacing.c
+++ ButtonInterfacing.c
@@ -1,4 +1,4 @@
-/*
+/*https://github.com/anokhramesh/Button-Toggle-PIC16F627A
  * File:   ButtonInterfacing.c
  * Author:Anokhramesh
  *
@@ -7,21 +7,26 @@
 #include <xc.h>
 __CONFIG(FOSC_INTOSCIO & WDTE_OFF & PWRTE_ON & MCLRE_ON & BOREN_ON & LVP_OFF & CPD_OFF & CP_OFF);//PIC configuration Bit setting
 #define _XTAL_FREQ 4000000// defined crystal frequency
-int i=3;
-
-
+int i=3;// initial value of global variable of i is = 3
+// connect Push button to pin  PORT B0  pull down to ground with 10K resister
+// connect LED to pin  PORT RB3
+//Connect High Active relay to PORT RB2 
 void main(void) {
-    TRISBbits.TRISB0=1;// connect Push button to pin  PORT B0  pulldown to ground with 10K resister
-    TRISBbits.TRISB1=0;// connect LED to pin  PORT B1
+    TRISBbits.TRISB0=1;//Set the PORTB Pin RB0 as an INPUT
+    TRISBbits.TRISB2=0;//Set the PORTB PIn RB2 as an OUTPUT
+    TRISBbits.TRISB3=0;//Set the PORTB PIn RB3 as an OUTPUT
+    RB2=0;// set the initial value of RB2 to LOW(RElay OFF)
+    RB3=1;// set the initial value of RB3 to HIGH(green LED ON)
     while(1)
-    { if ((RB0 ==1)&&(i==3))//if button is Pressed
-    {while(RB0==1){RB1=1;
-        i=1;}
+        
+    { if ((RB0 ==1)&&(i==3))//if button is Pressed and the value of variable i is equal to 3 then 
+    {while(RB0==1){RB2=1;RB3=0;// Turn ON the Relay connected to PIN RB2 and Turn OFF the LED COnnected to PIN RB3
+        i=1;}// store the value to variable i = 1
         
     }
-    if((RB0==1)&&(i==1))
-        while(RB0==1){RB1=0;
-            i=3;}
+    if((RB0==1)&&(i==1))//if button is Pressed and the value of variable i is equal to 1 then 
+        while(RB0==1){RB2=0;RB3=1;// Turn OFF the Relay connected to PIN RB2and Turn ON the LED COnnected to PIN RB3
+            i=3;}// restore the value of variable i = 3
             
     }
     
